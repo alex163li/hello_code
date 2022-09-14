13ad5f4c31e0c0b3d2f9ca531146d86cb788cd69
Alex <lialin163@yandex.ru>
2022-09-14 13:02:52 +0300

    Example_IfElse

---
+++ Example_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "алексей")
+{
+    Console.WriteLine("Здравствуй мой господин");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
